8e9d0f23208412cf47096e053f21c269cec460d8
John Lloyd Paranal <john.paranal@novare.com.hk>
2023-05-19 14:23:05 +0800

    filter task

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useRef } from "react";
 import TodoItem from "./todoItem";
-import './App.css'
+import "./App.css";
 
 export type TodoItemType = {
   id: number;
@@ -8,9 +8,18 @@ export type TodoItemType = {
   checked: boolean;
 };
 
+enum TodoVisibility {
+  All = "ALL",
+  Active = "ACTIVE",
+  Completed = "COMPLETED",
+}
+
 function App() {
   const [task, setTask] = useState("");
   const [todos, setTodos] = useState<TodoItemType[]>([]);
+  const [visibility, setVisibility] = useState<TodoVisibility>(
+    TodoVisibility.All
+  );
   const taskRef = useRef<HTMLInputElement>(null);
 
   const onTodoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -19,60 +28,92 @@ function App() {
 
   const checkedTodosCount = todos.filter((t) => t.checked).length;
 
+  let visibleTodos = todos;
+  if (visibility === TodoVisibility.Active) {
+    visibleTodos = todos.filter((t) => !t.checked);
+  } else if (visibility === TodoVisibility.Completed) {
+    visibleTodos = todos.filter((t) => t.checked);
+  }
+
   return (
     <>
-
+      <header>
         <h1>My Todo List</h1>
-   
-
-      <div className="new-todo">
-          <label htmlFor="name">Enter a new task:</label>
-          <div className="new-todo__h">
-          <input type="text" ref={taskRef} value={task} onChange={onTodoChange} />
-    
-        <button
-        onClick={() => {
-          setTodos((todos) => [
-            ...todos,
-            { id: new Date().getTime(), task, checked: false },
-          ]);
-          setTask("");
-          taskRef.current?.focus();
-        }}
-      >
-        ADD
-      </button>
-      <button
-        onClick={() => {
-          setTodos([]);
-        }}
-      style={{fontSize:"15px", marginLeft:"3px"}}>
-        Clear Todos
-      </button>
-      <span>
+        <span>
           Complete: {checkedTodosCount}/{todos.length}
         </span>
+      </header>
+
+      <hr/>
+      <div className="filtering">
+        <button
+          onClick={() => setVisibility(TodoVisibility.All)}
+        >
+          All
+        </button>
+        <button
+          onClick={() => setVisibility(TodoVisibility.Active)}
+        >
+          Active
+        </button>
+        <button
+          onClick={() => setVisibility(TodoVisibility.Completed)}
+        >
+          Completed
+        </button>
       </div>
+      <hr/>
+      <br/>
+
+      <div className="new-todo">
+        <label htmlFor="name">Enter a new task:</label>
+        <div className="new-todo__h">
+          <input
+            type="text"
+            ref={taskRef}
+            value={task}
+            onChange={onTodoChange}
+          />
+
+          <button
+            onClick={() => {
+              setTodos((todos) => [
+                ...todos,
+                { id: new Date().getTime(), task, checked: false },
+              ]);
+              setTask("");
+              taskRef.current?.focus();
+            }}
+          >
+            +
+          </button>
+          <button
+            onClick={() => {
+              setTodos([]);
+            }}
+            style={{ fontSize: "10px", marginLeft: "3px" }}
+          >
+            Clear Todos
+          </button>
+        </div>
       </div>
 
       <ul className="todos">
-        {todos.map((todo) => (
+        {visibleTodos.map((todo) => (
           <TodoItem
             key={todo.id}
             todo={todo}
             onCheckedChanged={(id, checked) => {
               setTodos((todos) =>
-                todos.map((t) =>
-                  t.id === id ? { ...t, checked } : t
-                )
+                todos.map((t) => (t.id === id ? { ...t, checked } : t))
               );
             }}
             onDelete={(id) => {
               setTodos((todos) => todos.filter((t) => t.id !== id));
             }}
           />
-      ))}
-    </ul>
+        ))}
+      </ul>
     </>
   );
 }
